af20eaa84d80eb0321b3d6a4d1e491f73f082d58
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-20 19:21:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
-# def reverse_each_word(string)
-#   array_of_string = string.split(" ")
-#   array_of_reversed_string = []
+def reverse_each_word(string)
+  array_of_string = string.split(" ")
+  array_of_reversed_string = []
   
-#   array_of_string.each do |word|
-#     array_of_reversed_string << word.reverse 
-#   end 
-#   return array_of_reversed_string.join(' ')
-# end 
+  array_of_string.each do |word|
+    array_of_reversed_string << word.reverse 
+  end 
+  return array_of_reversed_string.join(' ')
+end 
 
 def reverse_each_word(string)
   array_of_string = string.split(" ")
