bb1fe32c1529294fa3f9c1e603a73c1064453b25
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-20 19:28:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@
 def reverse_each_word(string)
   array_of_string = string.split(" ")
   array_of_string.collect do |word|
-    new_array << word.reverse
+    word.reverse
   end 
-  return new_array.join(' ')
 end 
